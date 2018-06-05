27de8a9d0be18ff8505c365996afadf065fde16c
Alex Turcu <turcu.alex5@gmail.com>
2018-06-05 14:58:56 +0300

    robots card added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'tachyons';
 import registerServiceWorker from './registerServiceWorker';
+import { robots } from './robots';
+import CardList from './CardList'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<CardList robots={robots}/>, document.getElementById('root'));
 registerServiceWorker();
