9302c081113ee19dce439a663c504773b0c14af3
Alex Turcu <turcu.alex5@gmail.com>
2018-06-20 10:30:05 +0300

    robots FTW

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'tachyons';
 import registerServiceWorker from './registerServiceWorker';
-import { robots } from './robots';
-import CardList from './CardList'
+import App from './App'
 
 
-ReactDOM.render(<CardList robots={robots}/>, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
